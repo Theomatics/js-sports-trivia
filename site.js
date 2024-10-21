a4116b9900047e147c737f809c840df64fcbb8ac
Bloedorn <500194031@fvtc.edu>
2024-10-21 13:41:02 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -8,14 +8,52 @@ const answersElement = document.querySelector('#answers')
 const nextQuestionElement = document.querySelector('#nextQuestion')
 
 // IIFE (so we can use async/await)
-(async () => {
+;(async () => {
 
-	// todo: create your "getNextQuestion" function
+	// Gets a question from the API
+	const getNextQuestion = async () => {
+		const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+		const response = await fetch(url)
+		const json = await response.json()
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+	}
 
-	// todo: create your "renderQuestion" function
+	// Render the question and possible answers onto the webpage
+	const renderQuestion = ({ question, answers, correct }) => { 
+		// render question
+		questionElement.textContent = decodeHtml(question)
 
-	// todo: add the event listener to the "nextQuestion" button
+		// render the possible answers
+		answersElement.innerHTML = ''
+		answers.forEach(answer => {
+			const answerOption = document.createElement('button')
+			answerOption.textContent = answer
+			answersElement.append(answerOption)
 
+			// add functionality to each possible answer
+			answerOption.addEventListener('click', () => {
+				if (answer === correct) {
+					answerOption.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return
+				}
+				
+				answerOption.disabled = true
+				alert('Incorrect!')
+			})
+		});
+	}
+	
+
+	// add functionality for the next question button
+	nextQuestionElement.addEventListener('click', async () => {
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	})
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
